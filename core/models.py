13ff818a16cbf3088e1a9c21e893f69e467b1c09
Aditya060 <51037240+Aditya060@users.noreply.github.com>
2024-09-11 01:18:21 +0530

    Update models.py
    Added unique id field

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 class User(models.Model):
     PANEL_CHOICES = [
@@ -14,6 +15,7 @@ class User(models.Model):
     job = models.CharField(max_length=100, default="")  # Change default=None to empty string
     company = models.CharField(max_length=100, null=True, blank=True)  # Allow null and blank for company
     panel = models.PositiveSmallIntegerField(choices=PANEL_CHOICES, verbose_name="Which panel would you like to attend", default=1)  # Set a valid default
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True,)
 
     def __str__(self):
         return f"{self.name} - {dict(self.PANEL_CHOICES).get(self.panel, 'Not selected')}"
