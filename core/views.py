219446359f5198945fced3ecfa9b091fb69379c5
Aditya060 <51037240+Aditya060@users.noreply.github.com>
2024-09-11 15:35:33 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -8,9 +8,20 @@ def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            print('form has not been saved yet')    
+            # Save the user to the database
+            user = form.save()
+
+            # Generate the QR code for the user's unique_id
+            
+            print('form has been saved')
+            # Send the QR code via email to the user's email address
+            send_qr_email(user.email, user.name, user.id, user.unique_id)
+            print('email has been sent')
+
             return redirect('success')
     else:
+        print('invalid form')
         form = RegistrationForm()
 
     return render(request, 'core/register.html', {'form': form})
