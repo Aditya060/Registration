10baf3641e4e06d942429b01251dbe88a70b18e4
Aditya060 <51037240+Aditya060@users.noreply.github.com>
2024-10-01 03:44:49 +0530

    Update models.py

--- core/models.py
+++ core/models.py
@@ -16,6 +16,16 @@ class User(models.Model):
     company = models.CharField(max_length=100, null=True, blank=True)  # Allow null and blank for company
     panel = models.PositiveSmallIntegerField(choices=PANEL_CHOICES, verbose_name="Which panel would you like to attend", default=1)  # Set a valid default
     unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True,)
+    
 
     def __str__(self):
         return f"{self.name} - {dict(self.PANEL_CHOICES).get(self.panel, 'Not selected')}"
+
+
+class ScannedQRCode(models.Model):
+    uid = models.CharField(max_length=255, unique=True)
+    status = models.IntegerField(default=1)
+
+    def _str_(self):
+        return self.uid
+
